fef1316ebe9bde6de14bdf162659e8170cdf99ec
Zakaria Kofiro <zkofiro@zendesk.com>
2022-07-06 02:22:20 -0500

    wip

---
+++ cert_scanner/web_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/index' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+ 
+app.run(host='localhost', port=5000)
